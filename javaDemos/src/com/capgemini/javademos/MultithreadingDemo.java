4b10eb62382587c061a68e48cbd928a3b5b860e7
DELL <DELL@DESKTOP-NA77PPN>
2022-11-15 11:08:19 +0530

    first commit

---
+++ javaDemos/src/com/capgemini/javademos/MultithreadingDemo.java
@@ -0,0 +1,49 @@
+package com.capgemini.javademos;
+class Eclipse extends Thread{
+	
+	public void run()
+	{
+		System.out.println("Eclipse Thread name is"+ Thread.currentThread().getName());
+	}
+}
+
+class calculator extends Thread
+{
+	public void run()
+	{
+		System.out.println("calculator Thread name is"+ Thread.currentThread().getName());
+	}
+
+}
+
+class Chrome extends Thread{
+	public void run()
+	{
+		System.out.println("Chrome Thread name is"+ Thread.currentThread().getName());
+	}
+}
+
+class OneNote extends Thread{
+	public void run()
+	{
+		System.out.println("OneNote Thread name is"+ Thread.currentThread().getName());
+	}
+
+}
+
+public class MultithreadingDemo {
+
+	public static void main(String[] args) {
+		Eclipse e=new Eclipse();	
+	    e.start();
+	    
+	    calculator c=new calculator();	
+	    c.start();
+	
+	    Chrome ch=new Chrome();	
+	    ch.start();
+	    
+	    OneNote on=new OneNote();	
+	    on.start();
+	}}
+		
